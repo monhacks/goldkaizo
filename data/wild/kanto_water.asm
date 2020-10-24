040c0d0633a0722f05aabb7fb0a2a61dd6f53482
pinksylveon <shakibafzali@gmail.com>
2020-10-23 23:23:21 -0400

    Finished editing kanto water mons

--- data/wild/kanto_water.asm
+++ data/wild/kanto_water.asm
@@ -4,146 +4,146 @@ KantoWaterWildMons:
 
 	map_id ROUTE_4
 	db 4 percent ; encounter rate
-	db 10, GOLDEEN
-	db 5, GOLDEEN
-	db 10, SEAKING
+	db 70, KINGLER
+	db 69, GYARADOS
+	db 70, SEAKING
 
 	map_id ROUTE_6
 	db 2 percent ; encounter rate
-	db 10, PSYDUCK
-	db 5, PSYDUCK
-	db 10, GOLDUCK
+	db 70, GOLDUCK
+	db 70, BLASTOISE
+	db 72, POLIWRATH
 
 	map_id ROUTE_9
 	db 4 percent ; encounter rate
-	db 15, GOLDEEN
-	db 10, GOLDEEN
-	db 15, SEAKING
+	db 68, FERALIGATR
+	db 68, SEAKING
+	db 68, QUAGSIRE
 
 	map_id ROUTE_10_NORTH
 	db 4 percent ; encounter rate
-	db 15, GOLDEEN
-	db 10, GOLDEEN
-	db 15, SEAKING
+	db 68, FERALIGATR
+	db 68, SEAKING
+	db 68, DEWGONG
 
 	map_id ROUTE_12
 	db 6 percent ; encounter rate
-	db 25, TENTACOOL
-	db 25, QUAGSIRE
-	db 25, TENTACRUEL
+	db 69, CLOYSTER
+	db 69, SLOWBRO
+	db 70, TENTACRUEL
 
 	map_id ROUTE_13
 	db 6 percent ; encounter rate
-	db 25, TENTACOOL
-	db 25, QUAGSIRE
-	db 25, TENTACRUEL
+	db 69, VAPOREON
+	db 69, KINGDRA
+	db 70, TENTACRUEL
 
 	map_id ROUTE_19
 	db 6 percent ; encounter rate
-	db 35, TENTACOOL
-	db 30, TENTACOOL
-	db 35, TENTACRUEL
+	db 74, TENTACRUEL
+	db 74, OCTILLERY
+	db 75, MANTINE
 
 	map_id ROUTE_20
 	db 6 percent ; encounter rate
-	db 35, TENTACOOL
-	db 30, TENTACOOL
-	db 35, TENTACRUEL
+	db 74, OMASTAR
+	db 74, KABUTOPS
+	db 75, LAPRAS
 
 	map_id ROUTE_21
 	db 6 percent ; encounter rate
-	db 35, TENTACOOL
-	db 30, TENTACOOL
-	db 35, TENTACRUEL
+	db 64, TENTACRUEL
+	db 64, LANTURN
+	db 65, SEAKING
 
 	map_id ROUTE_22
 	db 2 percent ; encounter rate
-	db 10, POLIWAG
-	db 5, POLIWAG
-	db 10, POLIWHIRL
+	db 62, POLIWRATH
+	db 62, SEAKING
+	db 64, POLIWHIRL
 
 	map_id ROUTE_24
 	db 4 percent ; encounter rate
-	db 10, GOLDEEN
-	db 5, GOLDEEN
-	db 10, SEAKING
+	db 67, POLITOED
+	db 67, SLOWKING
+	db 69, SEAKING
 
 	map_id ROUTE_25
 	db 4 percent ; encounter rate
-	db 10, GOLDEEN
-	db 5, GOLDEEN
-	db 10, SEAKING
+	db 67, POLITOED
+	db 67, SLOWKING
+	db 69, SEAKING
 
 	map_id ROUTE_26
 	db 6 percent ; encounter rate
-	db 30, TENTACOOL
-	db 25, TENTACOOL
-	db 30, TENTACRUEL
+	db 58, POLITOED
+	db 58, SLOWKING
+	db 60, SEAKING
 
 	map_id ROUTE_27
 	db 6 percent ; encounter rate
-	db 20, TENTACOOL
-	db 15, TENTACOOL
-	db 20, TENTACRUEL
+	db 32, TENTACOOL
+	db 33, TENTACOOL
+	db 33, TENTACRUEL
 
 	map_id TOHJO_FALLS
 	db 4 percent ; encounter rate
-	db 20, GOLDEEN
-	db 20, SLOWPOKE
-	db 20, SEAKING
+	db 34, GOLDEEN
+	db 32, KRABBY
+	db 34, SEAKING
 
 	map_id ROUTE_28
 	db 2 percent ; encounter rate
-	db 40, POLIWAG
-	db 35, POLIWAG
-	db 40, POLIWHIRL
+	db 81, POLIWRATH
+	db 81, QUAGSIRE
+	db 82, GOLDUCK
 
 	map_id PALLET_TOWN
 	db 6 percent ; encounter rate
-	db 35, TENTACOOL
-	db 30, TENTACOOL
-	db 35, TENTACRUEL
+	db 63, TENTACOOL
+	db 64, TENTACRUEL
+	db 64, TENTACOOL
 
 	map_id VIRIDIAN_CITY
 	db 2 percent ; encounter rate
-	db 10, POLIWAG
-	db 5, POLIWAG
-	db 10, POLIWHIRL
+	db 63, GYARADOS
+	db 63, GOLDUCK
+	db 65, SEAKING
 
 	map_id CERULEAN_CITY
 	db 4 percent ; encounter rate
-	db 10, GOLDEEN
-	db 5, GOLDEEN
-	db 10, SEAKING
+	db 66, SLOWKING
+	db 66, SLOWBRO
+	db 68, BLASTOISE
 
 	map_id VERMILION_CITY
 	db 6 percent ; encounter rate
-	db 35, TENTACOOL
-	db 30, TENTACOOL
-	db 35, TENTACRUEL
+	db 70, TENTACRUEL
+	db 71, FERALIGATR
+	db 73, TENTACRUEL
 
 	map_id CELADON_CITY
 	db 2 percent ; encounter rate
-	db 20, GRIMER
-	db 15, GRIMER
-	db 15, MUK
+	db 71, GRIMER
+	db 73, GRIMER
+	db 75, MUK
 
 	map_id FUCHSIA_CITY
 	db 2 percent ; encounter rate
-	db 20, MAGIKARP
-	db 15, MAGIKARP
-	db 10, MAGIKARP
+	db 74, KABUTOPS
+	db 74, POLIWRATH
+	db 75, POLITOED
 
 	map_id CINNABAR_ISLAND
 	db 6 percent ; encounter rate
-	db 35, TENTACOOL
-	db 30, TENTACOOL
-	db 35, TENTACRUEL
+	db 74, KINGDRA
+	db 74, OMASTAR
+	db 77, TENTACRUEL
 
 	map_id VERMILION_PORT
 	db 2 percent ; encounter rate
-	db 35, TENTACOOL
-	db 30, TENTACOOL
-	db 35, TENTACRUEL
+	db 70, OCTILLERY
+	db 71, BLASTOISE
+	db 73, TENTACRUEL
 
 	db -1 ; end
